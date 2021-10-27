7871d272118e04505663a857d0650803e7de2716
Daniel Rodriguez <drodriguez11375@gmail.com>
2021-10-27 03:32:17 -0400

    Renamed confirmedBookings variable

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
     type: String,
     required: true
   },
-  confirmed: [
+  confirmedBookings: [
     {
       type: Schema.Types.ObjectId,
       ref: 'ConfirmedBooking'
